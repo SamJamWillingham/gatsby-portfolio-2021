d374999f4a298c8d8f7e0b4eb4eda29faf4dd643
Sam Willingham <68390031+SamJamWillingham@users.noreply.github.com>
2021-04-10 12:09:35 -0400

    added projects and media queries for mobile

--- my-hello-world-starter/src/components/Buttons.jsx
+++ my-hello-world-starter/src/components/Buttons.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react"
 import Gatsby from "../images/gatsby";
 import GraphQL from "../images/graphQL";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHtml5, faSass,faBootstrap, faJs, faReact, faNodeJs } from "@fortawesome/free-brands-svg-icons"
+import BreatheMongoliaLogo from "../images/BreatheMongoliaLogo"
 import "../styles/buttons.css"
 
 function Buttons() {
@@ -10,6 +13,9 @@ const [openSkills, setOpenSkills] = useState(false);
 const [skillsChevron, setSkillsChevron] = useState('bi bi-chevron-down');
 const [openCert, setOpenCert] = useState(false);
 const [certChevron, setCertChevron] = useState('bi bi-chevron-down');
+
+const [openProject, setOpenProject] = useState(false);
+const [projectChevron, setProjectChevron] = useState('bi bi-chevron-down');
 
 
     return (
@@ -21,8 +27,8 @@ const [certChevron, setCertChevron] = useState('bi bi-chevron-down');
                 <i class="bi bi-person"></i>About<i className={aboutChevron}></i>
                 </button>
 
-                {openAbout ? (<div className={openAbout}>
-                        <p className="about-p">I am a React developer, currently working for Vahterus Oy 
+                {openAbout ? (<div className="about-p">
+                        <p> I am a React developer, currently working for Vahterus Oy 
                         and volunteering for the non-profit, Breathe Mongolia. Before I started web development, 
                         I was a high school teacher for five years! I have a bachelor's degree in English and four web development
                         certifications. I love connecting with people around the world and learning as much as I can from others.</p>
@@ -35,14 +41,14 @@ const [certChevron, setCertChevron] = useState('bi bi-chevron-down');
                 <i className="bi bi-code-slash"></i>Skills + Technologies<i className={skillsChevron}></i></button>
                 
                 {openSkills ? (<div id="tech-icon-wrapper">
-                    <i className="fab fa-html5"></i>
-                    <i className="fab fa-sass"></i>
-                    <i className="fab fa-bootstrap"></i>
-                    <i className="fab fa-js"></i>
-                    <i className="fab fa-react"></i>
+                    <FontAwesomeIcon icon={faHtml5} size="2x" />
+                    <FontAwesomeIcon icon={faSass} size="2x" />
+                    <FontAwesomeIcon icon={faBootstrap} size="2x" />
+                    <FontAwesomeIcon icon={faJs} size="2x" />
+                    <FontAwesomeIcon icon={faReact} size="2x" />
                     <Gatsby />
                     <GraphQL />
-                    <i class="fab fa-node-js"></i>
+                    <FontAwesomeIcon icon={faNodeJs} size="2x" />
                 </div>) : (<></>)}
 
                 <button 
@@ -50,15 +56,27 @@ const [certChevron, setCertChevron] = useState('bi bi-chevron-down');
                         setCertChevron(certChevron === 'bi bi-chevron-up' ? 'bi bi-chevron-down' : 'bi bi-chevron-up')
                         setOpenCert(!openCert)}}>
                 <i className="bi bi-award"></i>Certifications<i className={certChevron}></i></button>
-               {
-               openCert ? (<div id="certification-wrapper">
+               
+               {openCert ? (<div id="certification-wrapper">
                     <a href="https://www.shecodes.io/certificates/cb0ef7f22d93dfdb629e6f97198482b4">Introduction to Coding -SheCodes</a><br />
                     <a href="https://www.shecodes.io/certificates/1be2599ee6b555613e53f8e6a84b8328">Front End Development -SheCodes</a><br />
                     <a href="https://www.shecodes.io/certificates/370d835ecda3fd24895ade35bcb679ed">React Development -SheCodes</a><br />
                     <a href="https://www.freecodecamp.org/certification/samjamwillingham/responsive-web-design">Responsive Web Design -freeCodeCamp</a>
                 </div>) : (<></>)}
-
                 
+                <button 
+                    onClick={() => {
+                        setProjectChevron(projectChevron === 'bi bi-chevron-up' ? 'bi bi-chevron-down' : 'bi bi-chevron-up')
+                        setOpenProject(!openProject)}}>
+                <i class="bi bi-hammer"></i>Projects<i className={projectChevron}></i></button>
+                
+                {openProject ? (<div id="project-wrapper">
+                    <a href="https://clever-hugle-333d69.netlify.app/"><i class="bi bi-cloud-drizzle-fill"></i>React Weather App with SheCodes</a><br />
+                    <a href="https://ecstatic-leakey-e874b0.netlify.app/"><i class="bi bi-stars"></i>Tarot Card Drawer</a><br />
+                    <a href="https://lucid-clarke-3b55e8.netlify.app/"><i class="bi bi-peace"></i>Meditation Timer</a><br />
+                    <a href=""><BreatheMongoliaLogo />About Us page of Breathe Mongolia</a><br />
+                </div>) : (<></>)}
+
             </div>
     )
 }

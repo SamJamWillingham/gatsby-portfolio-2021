9698f43f09fb1e70ae5a9e1f01dc913c6119301a
Sam <samanthalf8@gmail.com>
2021-06-03 15:58:11 -0400

    added certification to cert list

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -64,6 +64,7 @@ function Buttons() {
                 <a href="https://www.shecodes.io/certificates/1be2599ee6b555613e53f8e6a84b8328">Front End Development -SheCodes</a><br />
                 <a href="https://www.shecodes.io/certificates/370d835ecda3fd24895ade35bcb679ed">React Development -SheCodes</a><br />
                 <a href="https://www.freecodecamp.org/certification/samjamwillingham/responsive-web-design">Responsive Web Design -freeCodeCamp</a>
+                <a href="https://www.udemy.com/certificate/UC-b994a18e-53bd-445e-8d3a-84f33bc4df8b/">Node, SQL, PostgreSQL - Mastering Backend Web Development</a>
             </div>) : (<></>)}
 
             <button

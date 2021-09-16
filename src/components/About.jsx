679034d1ecd3e9a7d93a35257ec5fa76225e82ec
Sam <samanthalf8@gmail.com>
2021-09-16 15:03:35 -0400

    starting to align content in about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 export default function About() {
 	return (<div className="expanded-div">
-		<h1>About</h1>
-		<p> I am currently a Junior Web Developer for Vahterus Oy. Before I started web development,
-			I was a high school teacher for five years! I have a bachelor's degree in English and four web development
-			certifications. I have been focusing predominatly on React and React frameworks, but I am currently learning PHP and Twig! I love connecting with people around the world and learning as much as I can from others!
-		</p>
+		<div className="expanded-flex-item">
+
+		</div>
+		<div className="expanded-flex-item">
+			<p>
+				I am currently a Junior Web Developer for Vahterus Oy. Before I started web development,
+				I was a high school teacher for five years! I have a bachelor's degree in English and four web development
+				certifications. I have been focusing predominatly on React and React frameworks, but I am currently learning PHP and Twig! I love connecting with people around the world and learning as much as I can from others!
+			</p>
+		</div>
+
 	</div>)
 }

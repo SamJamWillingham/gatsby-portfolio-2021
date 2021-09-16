6cafee54edf2f19647cf316c1030c2747196857e
Sam <samanthalf8@gmail.com>
2021-09-16 17:19:40 -0400

    testing plugin for timeline; commented out content

--- src/components/XP.jsx
+++ src/components/XP.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+// import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+// import 'react-vertical-timeline-component/style.min.css';
+// import Briefcase from "../images/icons/briefcase_icon";
 
 export default function XP() {
+
 	return (<div className="expanded-div">
-		<h1>Work Experience</h1>
-		<p>
-			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, tenetur. Assumenda quas voluptatem magnam aperiam, sequi cupiditate at, odio, nam commodi vero quo quis nulla minus nisi eligendi. Et, veritatis?
-		</p>
+		<div className="expanded-flex-item">
+			<h1>Work Experience</h1>
+		</div>
+		<div className="expanded-flex-item" >
+			{/* <VerticalTimeline>
+
+				<VerticalTimelineElement
+					className="vertical-timeline-element"
+					contentStyle={{ background: 'rgba(255, 255, 255, 0.4)' }}
+					contentArrowStyle={{ borderRight: '4px solid  rgb(33, 150, 243)' }}
+					date="2011 - present"
+					iconStyle={{ background: 'rgba(255, 255, 255, 0.4)' }}
+					icon={<Briefcase />}
+				>
+					<h3 className="vertical-timeline-element-title">Teacher</h3>
+					<h4 className="vertical-timeline-element-subtitle">Southlake, TX</h4>
+					<p>
+						had SUCH A FUN TIME TEACHING KIDSSSSSS OMGGGG
+					</p>
+				</VerticalTimelineElement>
+
+			</VerticalTimeline> */}
+		</div>
 	</div>)
 }

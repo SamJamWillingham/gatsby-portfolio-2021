a632de4b1c9fcaa7096c96437710d2f3cdd87089
Sam <samanthalf8@gmail.com>
2021-09-16 16:41:41 -0400

    updated gatsby; finished the About card with img, business card, and short bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import selfie from "../images/square_crop.jpg"
 
 export default function About() {
 	return (<div className="expanded-div">
 		<div className="expanded-flex-item">
-
+			<div className="inner-card">
+				<div className="card-item">
+					<img src={selfie} alt="Sam selfie" />
+				</div>
+				<div className="card-item">
+					<h2>Sam Fields Willingham</h2>
+					<p>she/they</p>
+					<p>Charlotte, NC</p>
+					<a href="hello@samjam.dev">hello@samjam.dev</a>
+				</div>
+			</div>
 		</div>
-		<div className="expanded-flex-item">
+		<div className="expanded-flex-item add-padding">
 			<p>
 				I am currently a Junior Web Developer for Vahterus Oy. Before I started web development,
-				I was a high school teacher for five years! I have a bachelor's degree in English and four web development
-				certifications. I have been focusing predominatly on React and React frameworks, but I am currently learning PHP and Twig! I love connecting with people around the world and learning as much as I can from others!
+				I was a high school teacher for five years! I have a bachelor's degree in English and five web development
+				certifications. I love connecting with people around the world and learning as much as I can from others!
 			</p>
 		</div>
 

a581a5d4b0f07c253dc8b5c42737fa3c8d9bb75c
Sam <samanthalf8@gmail.com>
2021-09-16 11:47:29 -0400

    each block now expands on click to take up whole grid space

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,3 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-  plugins: [
-    `gatsby-plugin-fontawesome-css`
-  ],
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
-import React from "react"
-import Briefcase from "../images/icons/briefcase_icon"
+import React, { useState } from "react"
 import Person from "../images/icons/person_icon"
+import Briefcase from "../images/icons/briefcase_icon"
+import About from "../components/About"
 import Code from "../images/icons/code_icon"
 import Award from "../images/icons/award_icon"
 import Chat from "../images/icons/chat_icon"
@@ -9,34 +10,91 @@ import "../styles/index.css"
 
 
 export default function Home() {
+	const [aboutClicked, setAboutClicked] = useState("false");
+	const handleAboutClick = () => {
+		setAboutClicked(!aboutClicked);
+	}
+
+	const [xpClicked, setXpClicked] = useState("false");
+	const handleXpClick = () => {
+		setXpClicked(!xpClicked);
+	}
+
+	const [skillsClicked, setSkillsClicked] = useState("false");
+	const handleSkillsClick = () => {
+		setSkillsClicked(!skillsClicked);
+	}
+
+	const [certsClicked, setCertsClicked] = useState("false");
+	const handleCertsClick = () => {
+		setCertsClicked(!certsClicked);
+	}
+
+	const [socialClicked, setSocialClicked] = useState("false");
+	const handleSocialClick = () => {
+		setSocialClicked(!socialClicked);
+	}
+
+	const [hobbiesClicked, setHobbiesClicked] = useState("false");
+	const handleHobbiesClick = () => {
+		setHobbiesClicked(!hobbiesClicked);
+	}
+
 	return <div className="App theme-light">
 		<div className="name">
 			{`<samJam />`}
 		</div>
 		<div className="grid-wrapper">
-			<div className="grid-block about">
-				<Person />
-				About
+			<div className={aboutClicked ? "grid-block about" : "grid-block about clicked"}
+				onClick={handleAboutClick}>
+				{aboutClicked ? (
+					<div className="pop">
+						<Person />
+						<h1>About</h1>
+					</div>
+				) : (<About />)}
+
+
 			</div>
-			<div className="grid-block xp">
-				<Briefcase />
-				Work Experience
+			<div className={xpClicked ? "grid-block xp" : "grid-block xp clicked"}
+				onClick={handleXpClick}>
+				<div className="pop">
+					<Briefcase />
+					<h1>Work Experience</h1>
+				</div>
+
 			</div>
-			<div className="grid-block skills">
-				<Code />
-				Programming skills
+			<div className={skillsClicked ? "grid-block skills" : "grid-block skills clicked"}
+				onClick={handleSkillsClick}>
+				<div className="pop">
+					<Code />
+					<h1>Programming skills</h1>
+				</div>
+
 			</div>
-			<div className="grid-block certs">
-				<Award />
-				Certifications
+			<div className={certsClicked ? "grid-block certs" : "grid-block certs clicked"}
+				onClick={handleCertsClick}>
+				<div className="pop">
+					<Award />
+					<h1>Certifications</h1>
+				</div>
+
 			</div>
-			<div className="grid-block social">
-				<Chat />
-				Follow me
+			<div className={socialClicked ? "grid-block social" : "grid-block social clicked"}
+				onClick={handleSocialClick}>
+				<div className="pop">
+					<Chat />
+					<h1>Follow me</h1>
+				</div>
+
 			</div>
-			<div className="grid-block hobbies">
-				<Joystick />
-				Interests &amp; Hobbies
+			<div className={hobbiesClicked ? "grid-block hobbies" : "grid-block hobbies clicked"}
+				onClick={handleHobbiesClick}>
+				<div className="pop">
+					<Joystick />
+					<h1>Interests &amp; Hobbies</h1>
+				</div>
+
 			</div>
 		</div>
 	</div >

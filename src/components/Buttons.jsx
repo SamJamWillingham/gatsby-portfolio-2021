5786f2ab15365844e8f6b8737362aaef9a60b970
Sam <samanthalf8@gmail.com>
2021-09-27 15:51:37 -0400

    Finished the new progress bars for skills section; mobile styles for section with react-responsive;
    added more languages, animated the progress bars, installed react-responsive

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,6 +3,8 @@ import About from "../components/About"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "../styles/buttons.css"
 import TechProgressBars from "./TechProgressBars"
+import Certifications from "./Certifications"
+
 
 function Buttons() {
 	const [openAbout, setOpenAbout] = useState(false);
@@ -41,14 +43,7 @@ function Buttons() {
 				}}>
 				<i className="bi bi-award"></i>Certifications<i className={certChevron}></i></button>
 
-			{openCert ? (<div id="certification-expanded">
-				<a href="https://www.shecodes.io/certificates/cb0ef7f22d93dfdb629e6f97198482b4">Introduction to Coding -SheCodes</a><br />
-				<a href="https://www.shecodes.io/certificates/1be2599ee6b555613e53f8e6a84b8328">Front End Development -SheCodes</a><br />
-				<a href="https://www.shecodes.io/certificates/370d835ecda3fd24895ade35bcb679ed">React Development -SheCodes</a><br />
-				<a href="https://www.freecodecamp.org/certification/samjamwillingham/responsive-web-design">Responsive Web Design -freeCodeCamp</a><br />
-				<a href="https://www.freecodecamp.org/certification/samjamwillingham/javascript-algorithms-and-data-structures">Javascript Algorithms and Data Structures -freeCodeCamp</a><br />
-				<a href="https://www.udemy.com/certificate/UC-b994a18e-53bd-445e-8d3a-84f33bc4df8b/">Node, SQL, PostgreSQL - Mastering Backend Web Development</a>
-			</div>) : (<></>)}
+			{openCert ? (<Certifications />) : (<></>)}
 
 		</div>
 	)
--- src/components/TechProgressBars.jsx
+++ src/components/TechProgressBars.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col, ProgressBar } from "react-bootstrap"
+import MediaQuery from 'react-responsive'
 import Html from "../images/icons/html_icon"
 import Css from "../images/icons/css_icon"
 import Js from "../images/icons/js_icon"
@@ -12,175 +13,295 @@ import GraphQL from "../images/icons/graphql_icon"
 import NodeJs from "../images/icons/nodejs_icon"
 import MySql from "../images/icons/mysql_icon"
 import PhpIcon from "../images/icons/php_icon"
+import Csharp from "../images/icons/cSharp_icon";
+import Markdown from "../images/icons/markdown_icon";
 
 
 export default function TechProgressBars() {
+
 	return (
 		<div id="tech-expanded">
-			<Container>
-				<Row>
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<Html />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={95} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<Css />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={96} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-				</Row>
+			<MediaQuery minWidth={1150}>
+				< Container >
+					<Row>
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<Html />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={95} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<Css />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={96} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+					</Row>
 
-				<Row className="add-padding">
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<Js />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={85} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<Typescript />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={70} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-				</Row>
+					<Row className="add-padding">
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<Js />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={85} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<Typescript />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={70} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+					</Row>
 
-				<Row className="add-padding">
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<Sass />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={96} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<ReactIcon />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={80} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-				</Row>
+					<Row className="add-padding">
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<Sass />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={96} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<ReactIcon />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={85} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+					</Row>
 
-				<Row className="add-padding">
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<BootstrapIcon />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={80} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<GatsbyIcon />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={65} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-				</Row>
+					<Row className="add-padding">
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<BootstrapIcon />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={80} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<GatsbyIcon />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={65} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+					</Row>
 
-				<Row className="add-padding">
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<NodeJs />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={80} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<GraphQL />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={65} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-				</Row>
+					<Row className="add-padding">
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<NodeJs />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={80} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<GraphQL />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={75} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+					</Row>
 
-				<Row>
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<MySql />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={55} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-					<Col className="col-6">
-						<Row>
-							<Col className="col-1">
-								<PhpIcon />
-							</Col>
-							<Col className="col-11 add-padding">
-								<ProgressBar now={55} striped variant="warning" />
-							</Col>
-						</Row>
-					</Col>
-				</Row>
+					<Row className="add-padding">
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<MySql />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={55} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<PhpIcon />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={55} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+					</Row>
 
-				<Row>
-					<Col>
+					<Row className="add-padding">
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<Csharp />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={30} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+						<Col className="col-6">
+							<Row>
+								<Col className="col-1">
+									<Markdown />
+								</Col>
+								<Col className="col-11 add-padding">
+									<ProgressBar animated now={90} striped variant="warning" />
+								</Col>
+							</Row>
+						</Col>
+					</Row>
+				</Container >
 
-					</Col>
-					<Col>
-
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-
-					</Col>
-					<Col>
-
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-
-					</Col>
-					<Col>
-
-					</Col>
-				</Row>
-			</Container>
-		</div>
+			</MediaQuery>
+			<MediaQuery maxWidth={1150}>
+				<Container>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<Html />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={95} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<Css />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={96} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<Js />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={85} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<Typescript />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={70} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<Sass />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={96} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<ReactIcon />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={85} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<BootstrapIcon />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={80} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<GatsbyIcon />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={65} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<NodeJs />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={80} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<GraphQL />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={75} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<MySql />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={55} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<PhpIcon />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={55} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<Csharp />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={30} striped variant="warning" />
+						</Col>
+					</Row>
+					<Row className="add-padding">
+						<Col className="col-1">
+							<Markdown />
+						</Col>
+						<Col className="col-11 add-padding">
+							<ProgressBar animated now={90} striped variant="warning" />
+						</Col>
+					</Row>
+				</Container>
+			</MediaQuery>
+		</div >
 	)
 }
